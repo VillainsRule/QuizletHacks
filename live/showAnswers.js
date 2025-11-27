3979695ffd1af27b3efaa4e13671de0a245c1700
VR <101288516+VillainsRule@users.noreply.github.com>
2025-11-27 09:26:12 -0500

    fix images on live mode (closes #3)

--- live/showAnswers.js
+++ live/showAnswers.js
@@ -3,20 +3,27 @@
     if (!target) return;
     const targetFiber = target[Object.keys(target).find(e => e.startsWith('__reactFiber$'))];
     const stateNode = targetFiber.child.pendingProps;
-    const cardSides = stateNode.currentQuestion.cardSides.map(e => e.media[0]);
+    const cardSides = stateNode.currentQuestion.cardSides.map(e => e.media);
     const questionType = stateNode.options.answerWith;
 
-    const side1 = cardSides[0].plainText;
-    const side2 = cardSides[1].plainText;
-
     const side1IsQuestion = questionType === 'definition';
-    const answerText = side1IsQuestion ? side2 : side1;
+    const answerSide = side1IsQuestion ? cardSides[1] : cardSides[0];
+    const answerIsImage = answerSide[0].languageCode === 'photo';
 
-    let answerOpts = document.querySelector('div[data-testid="normalPrompt"]').parentElement.children[1];
+    if (answerIsImage) {
+        const answerOpts = document.querySelectorAll('.Image-image');
+        answerOpts.forEach((opt) => {
+            const isCorrect = opt.getAttribute('style').includes(answerSide[1].url);
+            opt.style.border = isCorrect ? '3px solid lime' : '3px solid red';
+            opt.style.borderRadius = '3px';
+        });
+    } else {
+        const answerOpts = document.querySelector('div[data-testid="normalPrompt"]').parentElement.children[1];
 
-    let answerCard = [...answerOpts.children].find((opt) => opt.children[0].innerText === answerText);
-    let wrongCards = [...answerOpts.children].filter(a => a !== answerCard);
+        const answerCard = [...answerOpts.children].find((opt) => opt.children[0].innerText === answerSide[0].plainText);
+        const wrongCards = [...answerOpts.children].filter(a => a !== answerCard);
 
-    if (answerCard) answerCard.style.color = 'lime';
-    wrongCards.forEach((card) => card.style.color = 'red');
-}, 100))();
+        if (answerCard) answerCard.style.color = 'lime';
+        wrongCards.forEach((card) => card.style.color = 'red');
+    }
+}, 50))();
