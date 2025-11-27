bf7b8ce564bfac3805581d79132940c11ad64a5f
VR <101288516+VillainsRule@users.noreply.github.com>
2025-11-27 15:53:49 -0500

    fix blocks

--- blocks/showAnswer.js
+++ blocks/showAnswer.js
@@ -1,14 +1,15 @@
 (() => {
-    let setData = JSON.parse(__NEXT_DATA__.props.pageProps.dehydratedReduxStateKey);
-    let cards = setData.studyModesCommon.studiableData.studiableItems;
+    const setData = JSON.parse(__NEXT_DATA__.props.pageProps.dehydratedReduxStateKey);
+    const cards = setData.studyModesCommon.studiableData.studiableItems;
 
     document.body.insertAdjacentHTML('beforeend', `<style>label[class="AssemblyInput"]::placeholder { opacity: 0.5 }`);
 
     setInterval(() => {
-        let term = document.querySelector('p').innerText;
-        let card = cards.find((card) => card.cardSides.some((side) => side.media[0].plainText == term));
-        let otherSide = card.cardSides.find((side) => side.media[0].plainText !== term);
+        const term = document.querySelector('p').innerText;
+        const card = cards.find((card) => card.cardSides.some((side) => side.media[0].plainText == term));
+        if (!card) return;
 
-        document.querySelector('label[class="AssemblyInput"]').children[0].placeholder = 'answer: ' + otherSide.media[0].plainText;
+        const otherSide = card.cardSides.find((side) => side.media[0].plainText !== term);
+        document.querySelector('input').placeholder = 'answer: ' + otherSide.media[0].plainText;
     }, 100);
 })();
