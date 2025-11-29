a02d592583126073a117617fa81e685f79f3d21c
VR <101288516+VillainsRule@users.noreply.github.com>
2025-11-29 16:08:27 -0500

    make blocks work w/ images (closes #15)

--- blocks/showAnswer.js
+++ blocks/showAnswer.js
@@ -5,8 +5,9 @@
     document.body.insertAdjacentHTML('beforeend', `<style>label[class="AssemblyInput"]::placeholder { opacity: 0.5 }`);
 
     setInterval(() => {
+        const image = document.querySelector('img[src]')?.src;
         const term = document.querySelector('p').innerText;
-        const card = cards.find((card) => card.cardSides.some((side) => side.media[0].plainText == term));
+        const card = cards.find((card) => card.cardSides.some((side) => side.media[0].plainText == term && (image ? side.media[1]?.url?.replace('_m.', '.') === image : true)));
         if (!card) return;
 
         const otherSide = card.cardSides.find((side) => side.media[0].plainText !== term);
